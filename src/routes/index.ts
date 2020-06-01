673a8143b2f7095c4cc1b8a2eca12258d755928e
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-06-01 02:20:45 -0300

    chore: add plugins to solve import and types

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router, Context, Status } from 'https://deno.land/x/oak/mod.ts';
 
 const router = new Router();
 
-router.post('/', async (context: Context) => {
+router.get('/', async (context: Context) => {
   context.response.body = {
     status: Status.OK,
     message: 'Successfully',
